fcb5550ad2dd3a8b98a004965923209c09e575d2
Aljo <aljoemmanu22@gmail.com>
2024-04-11 18:03:05 +0530

    Added more generes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
-import {originals,  action } from './urls'; 
+import {originals, action, horror, comedy, romance } from './urls'; 
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Banner />
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title= 'Action' isSmall />
+      <RowPost  url={horror} title={'Horror Movies'}/>
+      <RowPost isSmall url={comedy} title={'Comedy Movies'} />
+      <RowPost  url={romance} title={'Romantic Movies'}/>
+      <Footer/>
     </div>
   );
 }
